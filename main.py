2855576eb1cb9c84db2cb9eef5012da565850b02
itz-mikey <abdullahumer370@gmail.com>
2025-07-07 10:24:28 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import random as rd
+hidden_num = rd.randint(1,100)
+usr_guess = int(input("Guess the Number From 1 to 100: "))
+atmpt = 1
+if usr_guess == hidden_num:
+    print("You WON!")
+    print("Attempt taken: 1 ")
+else:
+    while usr_guess != hidden_num:
+        if usr_guess > hidden_num:
+            if usr_guess > 100 :
+             print("Please Enter a Number less than or equal to 100.")
+            print("Wrong Guess, Try smaller number")
+        else:
+            if usr_guess < 1:
+                print("Please Enter a Number greeater than 0.")
+            print("Wrong Guess, Try larger number")
+        usr_guess = int(input("Guess the Number From 1 to 100: "))
+        atmpt += 1
+    print("You WON!")
+    print(f"Attempt taken: {atmpt}")
